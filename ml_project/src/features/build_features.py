d8652ba39346130cc3ae93c4b92cde55e6fa596c
Alex Emelyanov <holyketzer@gmail.com>
2021-05-01 20:48:42 +0300

    Fix data preprocessing

--- ml_project/src/features/build_features.py
+++ ml_project/src/features/build_features.py
@@ -1,15 +1,17 @@
 # -*- coding: utf-8 -*-
 import click
 import logging
+import os
 import pandas as pd
 
 from dotenv import find_dotenv, load_dotenv
 from os import listdir
 from os.path import isfile, join
 from pathlib import Path
+from sklearn.model_selection import train_test_split
 
+from src.config import config
 from src.logging import logger
-
 
 @click.command()
 @click.argument('input_filepath', type=click.Path(exists=True))
@@ -26,8 +28,17 @@ def process_file(f, input_filepath, output_filepath):
     out_file_path = join(output_filepath, f)
     df = pd.read_csv(join(input_filepath, f))
     df = df.dropna()
-    df.to_csv(out_file_path)
-    logger.info(f'Final data set saved at: {out_file_path}')
+
+    df_train, df_test = train_test_split(df, test_size=config['test_split'], random_state=config['random_state'])
+
+    path, ext = os.path.splitext(out_file_path)
+
+    save_to_file(df_train, path + "_train" + ext)
+    save_to_file(df_test, path + "_test" + ext)
+
+def save_to_file(df, path):
+    df.to_csv(path)
+    logger.info(f'Final data set part saved at: {path}')
 
 if __name__ == '__main__':
     # not used in this stub but often useful for finding various files
